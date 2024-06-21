adec40119a5ca8276d1910655044e32b0c2c56f6
VgTajdd <aduranddiaz@gmail.com>
2024-06-21 19:19:19 +0000

    Update tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,39 +1,46 @@
 #include <gtest/gtest.h>
 
 #include <common/IPlugin.h>
+#include <app/ICustomPlugin.h>
 #include <plugin_manager/IPluginManager.h>
 
 TEST( PluginManagerTest, PluginName )
 {
-    char pluginName[20];
-    if ( IPluginManager::Get()->loadPlugin( "pluginA" ) )
-    {
-        auto plugin = IPluginManager::Get()->getPlugin( "pluginA" );
-        strcpy( pluginName, plugin->name() );
-        IPluginManager::Get()->unloadPlugin( "pluginA" );
-    }
+	char pluginName[20];
+	if ( IPluginManager::Get()->loadPlugin( "pluginA" ) )
+	{
+		auto plugin = IPluginManager::Get()->getPlugin( "pluginA" );
+		strcpy( pluginName, plugin->name() );
+		IPluginManager::Get()->unloadPlugin( "pluginA" );
+	}
 
-    EXPECT_STREQ( "PluginA", pluginName );
+	EXPECT_STREQ( "PluginA", pluginName );
 }
 
 TEST( PluginManagerTest, DoSomething )
 {
-    const char* plugins[] = { "pluginA", "pluginB" };
+	const char* plugins[] = { "pluginA", "pluginB" };
 
-    float values[2];
-    size_t i{ 0 };
+	float values[2] = { 0.0f, 0.0f };
+	size_t i{ 0 };
 
-    for ( const auto pluginFilename : plugins )
-    {
-        if ( IPluginManager::Get()->loadPlugin( pluginFilename ) )
-        {
-            auto plugin = IPluginManager::Get()->getPlugin( pluginFilename );
-            plugin->sayHelloTo( "Agustin" );
-            values[i++] = plugin->doSomething( 4, 5 );
-            IPluginManager::Get()->unloadPlugin( pluginFilename );
-        }
-    }
+	for ( const auto pluginFilename : plugins )
+	{
+		if ( IPlugin* plugin = IPluginManager::Get()->loadPlugin( pluginFilename ) )
+		{
+			if ( ICustomPlugin* customPlugin = dynamic_cast<ICustomPlugin*>( plugin ) )
+			{
+				customPlugin->sayHelloTo( "Agustin" );
+				values[i++] = customPlugin->doSomething( 4, 5 );
+			}
+			else
+			{
+				std::cout << "Plugin does not implement ICustomPlugin." << std::endl;
+			}
+			IPluginManager::Get()->unloadPlugin( pluginFilename );
+		}
+	}
 
-    EXPECT_EQ( 9, values[0] );
-    EXPECT_EQ( 20, values[1] );
+	EXPECT_EQ( 9, values[0] );
+	EXPECT_EQ( 0, values[1] );
 }
